7371daf3366d3ddbfe0a10f3fb2081c09294b36c
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-02-18 19:08:06 +0300

    my redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { createStore } from "./createStore";
+import { increment, decrement } from "./redux/actions";
+import { rootReducer } from "./redux/rootReducer";
 import "./styles.css";
 
 const counter = document.getElementById("counter");
@@ -6,31 +9,21 @@ const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
 
-let state = 0;
-
-function render() {
-  counter.textContent = state.toString();
-}
+const store = createStore(rootReducer, 0);
 
 addBtn.addEventListener("click", () => {
-  state++;
-  render();
+  store.dispatch(increment());
 });
 
 subBtn.addEventListener("click", () => {
-  state--;
-  render();
+  store.dispatch(decrement());
 });
 
-asyncBtn.addEventListener("click", () => {
-  setTimeout(() => {
-    state++;
-    render();
-  }, 2000);
-});
+asyncBtn.addEventListener("click", () => {});
+
+store.subscribe(() => (counter.innerText = store.getState()));
+store.dispatch({ type: "INIT_COUNTER" });
 
 themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+  //   document.body.classList.toggle("dark");
 });
-
-render();
